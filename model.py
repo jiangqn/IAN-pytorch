d6caea1e631ae57ebfa0bb15c99517bc5211da8f
ThomasK427 <1935448858@qq.com>
2018-12-28 16:48:14 +0800

    update

--- model.py
+++ model.py
@@ -39,26 +39,25 @@ class IAN(nn.Module):
         self.hidden_size = config.hidden_size
         self.n_class = config.n_class
         self.l2_reg = config.l2_reg
-        self.dropout = config.dropout
         self.max_aspect_len = config.max_aspect_len
         self.max_context_len = config.max_context_len
 
         self.embedding = nn.Embedding(num_embeddings=self.vocab_size, embedding_dim=self.embedding_size)
-        self.embedding_dropout = nn.Dropout(self.dropout)
         self.aspect_lstm = nn.LSTM(input_size=self.embedding_size, hidden_size=self.hidden_size, batch_first=True)
         self.context_lstm = nn.LSTM(input_size=self.embedding_size, hidden_size=self.hidden_size, batch_first=True)
         self.aspect_attn = Attention(self.hidden_size, self.hidden_size)
         self.context_attn = Attention(self.hidden_size, self.hidden_size)
+        self.dropout = nn.Dropout(config.dropout)
         self.fc = nn.Linear(self.hidden_size * 2, self.n_class)
-        # self.embedding.weight.data.copy_(torch.from_numpy(config.embedding))
+        self.embedding.weight.data.copy_(torch.from_numpy(config.embedding))
 
     def forward(self, aspect, context, aspect_mask, context_mask):
         aspect = self.embedding(aspect)
-        # aspect = self.embedding_dropout(aspect)
+        # aspect = self.dropout(aspect)
         aspect_output, _ = self.aspect_lstm(aspect)
         aspect_avg = aspect_output.sum(dim=1, keepdim=False) / aspect_mask.sum(dim=1, keepdim=True)
         context = self.embedding(context)
-        # context = self.embedding_dropout(context)
+        # context = self.dropout(context)
         context_output, _ = self.context_lstm(context)
         context_avg = context_output.sum(dim=1, keepdim=False) / context_mask.sum(dim=1, keepdim=True)
         aspect_attn = self.aspect_attn(context_avg, aspect_output, aspect_mask).unsqueeze(1)
@@ -66,7 +65,9 @@ class IAN(nn.Module):
         context_attn = self.context_attn(aspect_avg, context_output, context_mask).unsqueeze(1)
         context_features = context_attn.matmul(context_output).squeeze()
         features = torch.cat([aspect_features, context_features], dim=1)
+        features = self.dropout(features)
         output = self.fc(features)
+        output = torch.tanh(output)
         return output
 
 class IanDataset(Dataset):
